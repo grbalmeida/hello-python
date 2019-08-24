7ca7b84b95bb7b79790eb77573a4788afbb14475
gilvan <g.r.almeida@live.com>
2019-08-24 20:33:14 -0300

    Comments

---
+++ programming-logic/comments.py
@@ -0,0 +1,18 @@
+# This is a comment
+
+print('First line')   # First line
+print('Second line')  # Second line
+print('Third line')   # Third line
+
+# Avoid obvious comments
+# Make comments that describe the functionality of the code
+
+'''
+  Docstrings
+  Multiline comment
+  Widely used in documentation
+'''
+
+"""
+  Docstrings work with single and double quotes
+"""
