f512244df3e1f1794cc5e572df5df254a6d99073
gilvan <g.r.almeida@live.com>
2019-08-24 23:43:50 -0300

    User input

---
+++ programming-logic/calculator.py
@@ -0,0 +1,5 @@
+first_number = input('Enter a number: ')
+second_number = input('Enter another number: ')
+result = float(first_number) + float(second_number)
+
+print(f'{first_number} + {second_number} = {result}')
---
+++ programming-logic/user-input.py
@@ -0,0 +1,6 @@
+name = input('What is your name? ')
+age = input('How old are you? ')
+year_of_birth = 2019 - int(age)
+
+print(f'{name} is {age} years old and born in year {year_of_birth}')
+
