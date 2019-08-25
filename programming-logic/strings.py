6a22f35f8df0fc5d1cbe2e604d8c60f9feee48e9
gilvan <g.r.almeida@live.com>
2019-08-24 22:21:19 -0300

    Strings

---
+++ programming-logic/strings.py
@@ -0,0 +1,22 @@
+"""
+  str - string
+"""
+
+print('Something')
+print('Single quotes')
+print("Double quotes")
+print('A string that is enclosed in quotation marks')
+
+print(type(123456))    #  int
+print(type('123456'))  #  str
+
+print('This is a \'string\'')
+print("This is a 'string'")
+print('This is a "string"')
+
+print('Line\nbreak')
+print(r'Line\nbreak')
+
+print('Phrase\twith\ttab')
+print(r'Phrase\twith\ttab')
+
