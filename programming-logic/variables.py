130c4bb7fd44e003282dfb7fe62da30917ea5f41
gilvan <g.r.almeida@live.com>
2019-08-24 23:13:22 -0300

    Variables

---
+++ programming-logic/variables.py
@@ -0,0 +1,18 @@
+name = 'Luiz'
+print('Name:', name, type(name))
+
+age = 32
+print('Age:', age, type(age))
+
+height = 1.80
+print('Height:', height, type(height))
+
+weight = 80
+print('Weight:', weight, type(weight))
+
+is_greater_than_18 = age > 18
+print('Is of legal age:', is_greater_than_18, type(is_greater_than_18))
+
+bmi = weight / height ** 2
+print(name, 'is', age, 'years old and his BMI is', bmi, sep=' ')
+
