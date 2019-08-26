a5bded848c28924774a2f237f5f5773a601870f9
gilvan <g.r.almeida@live.com>
2019-08-26 11:28:55 -0300

    Operator or

---
+++ programming-logic/conditional-structures/multiple-operators.py
@@ -0,0 +1,19 @@
+first_variable = 0
+second_variable = None
+third_variable = False
+fourth_variable = []
+fifth_variable = {}
+sixth_variable = 22
+seventh_variable = 'Luiz'
+
+variable = (
+  first_variable or 
+  second_variable or
+  third_variable or
+  fourth_variable or
+  fifth_variable or
+  sixth_variable or
+  seventh_variable
+)
+
+print(variable) # 22
---
+++ programming-logic/conditional-structures/operator-or.py
@@ -0,0 +1,6 @@
+name = input('What is your name? ')
+age = input('How old are you? ')
+
+
+print(name or 'You didn\'t enter the name')
+print(age or 'You didn\'t enter the age')
