168df517651e5f018132e0c706dbfef1063ae1bf
gilvan <g.r.almeida@live.com>
2019-08-26 11:16:27 -0300

    Ternary operation

---
+++ programming-logic/conditional-structures/ternary-operation.py
@@ -0,0 +1,8 @@
+is_user_logged_in = True
+message = 'User logged in' if is_user_logged_in else 'User needs to login'
+
+print(message)
+
+age = 18
+message = 'Can access' if age >= 18 else 'Cannot access'
+print(message)
