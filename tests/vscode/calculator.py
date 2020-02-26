4d9ddf2f7d104fdbc3aed2c88e50af19a39c1b63
gilvan <g.r.almeida@live.com>
2020-02-25 21:37:36 -0300

    Assertions

--- tests/vscode/calculator.py
+++ tests/vscode/calculator.py
@@ -1,2 +1,4 @@
 def sum(x, y):
+    assert isinstance(x, (int, float)), 'x must be int or float'
+    assert isinstance(y, (int, float)), 'y must be int or float'
     return x + y
--- tests/vscode/main.py
+++ tests/vscode/main.py
@@ -4,3 +4,13 @@ print(sum(10, 20))
 print(sum(-10, 20))
 print(sum(10.5, 20.5))
 print(sum(1.5, 2.5))
+
+try:
+    print(sum('15', 15))
+except AssertionError as e:
+    print(e)
+
+try:
+    print(sum(15, '15'))
+except AssertionError as e:
+    print(e)
